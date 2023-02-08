2e38ee1661e56153daa6aee29c3e2e321976bdea
Alejo Boga <alejoboga20@gmail.com>
2023-02-08 14:21:56 -0500

    signin form

--- src/pages/auth/SigninRoute.tsx
+++ src/pages/auth/SigninRoute.tsx
@@ -1,3 +1,38 @@
+import { Link } from 'react-router-dom';
+import logo from '../../../public/images/Logo.png';
+
 export const SigninRoute = () => {
-	return <div id='signin-page'>SigninRoute</div>;
+	return (
+		<div id='signin-page'>
+			<form action='/auth/signin' className='bg-white rounded-lg pt-10 px-4' noValidate>
+				<div className='w-full flex justify-center'>
+					<img src={logo} alt='blackmarket' />
+				</div>
+
+				<div className='flex flex-col mt-9'>
+					<label htmlFor='email'>Email</label>
+					<input type='email' id='email' formNoValidate />
+
+					<label htmlFor='password'>Password</label>
+					<input type='password' id='password' formNoValidate />
+
+					<button name='login' type='submit'>
+						Log in
+					</button>
+					<Link to='forgot-password' className='text-center'>
+						I forgot my password
+					</Link>
+				</div>
+			</form>
+
+			<div className='bg-white rounded-lg py-5 px-4 mt-4 text-center'>
+				<p>Don't have an account?</p>
+				<Link to='register'>
+					<button name='signup' className='mt-4'>
+						Sign up
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
--- src/routes/RootAuth.tsx
+++ src/routes/RootAuth.tsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router-dom';
 const RootAuth = () => {
 	return (
 		<div className='bg-auth-background w-screen h-screen bg-cover'>
-			<Outlet />
+			<main className='px-4 pt-10'>
+				<Outlet />
+			</main>
 		</div>
 	);
 };
